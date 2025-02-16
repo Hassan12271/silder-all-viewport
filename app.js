ce9813394cd78838b19005fe41562c84a0ef7f14
Hassan12271 <hassandanyal18@gmail.com>
2025-02-16 19:56:07 +0500

    done

---
+++ app.js
@@ -0,0 +1,20 @@
+// let Number = +prompt("Enter Your Wnated Number");
+// let length = +prompt("Enter Where Did You Want");
+
+// for (let i = 1; i <= length; i++){
+//     document.write(`${Number} x ${i} = ${Number * i} <br/>`);
+// }
+let check = prompt("Please Enter your City");
+
+let Cities = ['karachi'  , 'lahore' , 'peshawer' , 'Islamabad' , 'quata'];
+
+for (i = 0; i < Cities.length; i++){
+    if(Cities[i] === check){
+        console.log('your are good');
+        break;
+    }
+    else if(Cities[i] !== check){
+        console.log(`your are not a good`);
+    }
+    break;
+}
